d1d0f10a00ceb490a298be17d7e4de1232fa5314
Giuseppe Cuccu <giuse@blade.fritz.box>
2014-11-14 12:12:22 +0100

    Added graphics for background, ball and paddle

--- verybasic.rb
+++ verybasic.rb
@@ -7,17 +7,29 @@ require "slick.jar"
 java_import org.newdawn.slick.BasicGame
 java_import org.newdawn.slick.GameContainer
 java_import org.newdawn.slick.Graphics
+java_import org.newdawn.slick.Image
 java_import org.newdawn.slick.Input
 java_import org.newdawn.slick.SlickException
 java_import org.newdawn.slick.AppGameContainer
 
 class Demo < BasicGame
-  def render container, graphics
-    graphics.draw_string('JRuby Demo (ESC to quit)', 8, container.height - 30)
-  end
 
   # When Java is involved, always fill in the prototype  
   def init container
+    @bg = Image.new 'bg.png'
+    @ball = Image.new 'ball.png'
+    @paddle = Image.new 'paddle.png'
+    @paddle_x = 200
+    @ball_x = 200
+    @ball_y = 200
+    @ball_angle = 45
+  end
+
+  def render container, graphics
+    @bg.draw 0,0
+    @ball.draw @ball_x, @ball_y
+    @paddle.draw @paddle_x, 400
+    graphics.draw_string('JRuby Demo (ESC to quit)', 8, container.height - 30)
   end
 
   def update container, delta
